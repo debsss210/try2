0cddf66ee3d77c29a7af13bc2c9fefa2e4abbdae
debsss210 <debanjansinha.ec19@rvce.edu.in>
2020-10-01 01:40:05 +0530

    i

---
+++ js/tut2.js
@@ -0,0 +1,12 @@
+console.time("Your code took");
+console.log("Hello console");
+console.log(4+34);
+console.log(true);
+console.log([34,2,3,1]);
+console.log({harry: 'this', marks: 34})
+console.table({harry: 'this', marks: 34})
+console.warn("This is a warning")
+// console.clear()
+console.timeEnd("Your code took");
+console.assert(234<120, "Age>120 is not possible")
+console.error("This is an error")
